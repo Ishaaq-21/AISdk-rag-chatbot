d1ed1778ab6c48c01bd6eaced784f17a4c445f8d
isaac <ishak.hadj14@gmail.com>
2025-10-26 03:18:52 +0100

    build our initial chat interface

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { Fragment, useState } from "react";
+import { useChat } from "@ai-sdk/react";
+import {
+  Conversation,
+  ConversationContent,
+  ConversationEmptyState,
+  ConversationScrollButton,
+} from "@/components/ai-elements/conversation";
+import { Message, MessageContent } from "@/components/ai-elements/message";
+import {
+  PromptInput,
+  PromptInputBody,
+  type PromptInputMessage,
+  PromptInputSubmit,
+  PromptInputTextarea,
+  PromptInputTools,
+} from "@/components/ai-elements/prompt-input";
+import { Response } from "@/components/ai-elements/response";
+import { Loader } from "@/components/ai-elements/loader";
+import { MessageSquare } from "lucide-react";
+import { UIMessage } from "ai";
+
+const RAGPage = () => {
+  const [input, setInput] = useState("");
+  const { messages, sendMessage, status } = useChat();
+
+  const handleSubmit = (message: PromptInputMessage) => {
+    if (!message.text) return;
+
+    sendMessage({ text: message.text });
+    setInput("");
+  };
+  return (
+    <div className="max-w-4xl p-6 mx-auto size-full relative ">
+      <div className="flex flex-col h-full">
+        <Conversation className="h-full">
+          <ConversationContent>
+            {messages.length === 0 ? (
+              <ConversationEmptyState
+                icon={<MessageSquare className="size-12" />}
+                title="No messages yet"
+                description="Start a conversation to see messages here"
+              />
+            ) : (
+              messages.map((message: UIMessage) => (
+                <Message from={message.role} key={message.id}>
+                  <MessageContent>
+                    {message.parts.map((part, i) => {
+                      switch (part.type) {
+                        case "text": // we don't use any reasoning or tool calls in this example
+                          return (
+                            <Response key={`${message.id}-${i}`}>
+                              {part.text}
+                            </Response>
+                          );
+                        default:
+                          return null;
+                      }
+                    })}
+                  </MessageContent>
+                </Message>
+              ))
+            )}
+          </ConversationContent>
+          {(status === "submitted" || status === "streaming") && <Loader />}
+
+          <ConversationScrollButton />
+
+          <PromptInput className="" onSubmit={handleSubmit}>
+            <PromptInputBody>
+              <PromptInputTextarea
+                value={input}
+                onChange={(e) => {
+                  setInput(e.target.value);
+                }}
+              />
+            </PromptInputBody>
+            <PromptInputTools />
+            <PromptInputSubmit
+              className="cursor-pointer"
+              disabled={!input && !status}
+              status={status}
+            />
+          </PromptInput>
+        </Conversation>
+      </div>
+    </div>
+  );
+};
+
+export default RAGPage;
